4de069a5190ed1aec9b44d295b86c1db30228aff
kimyonghwan1996 <137673370+kimyonghwan1996@users.noreply.github.com>
2024-03-22 17:38:57 +0900

    240322커밋

--- Chapter01_XML/src/main/java/sample05/SjungjukImpl.java
+++ Chapter01_XML/src/main/java/sample05/SjungjukImpl.java
@@ -1,7 +1,8 @@
 package sample05;
 
+import java.util.Scanner;
+
 public class SjungjukImpl implements SungJuk {
-	public SjungjukImpl() {}
 	
 	private String name;
 	private int kor;
@@ -10,12 +11,30 @@ public class SjungjukImpl implements SungJuk {
 	private int tot;
 	private int avg;
 	
-	public void calc(int kor, int eng, int math) {
+	public SjungjukImpl() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("이름 : ");
+		name = sc.next();
+		
+		System.out.print("국어 : ");
+		kor = sc.nextInt();
+		
+		System.out.print("영어 : ");
+		eng = sc.nextInt();
+		
+		System.out.print("수학 : ");
+		math = sc.nextInt();
+	}
+	
+	@Override
+	public void calc() {
 		tot = kor + eng + math;
 		avg = tot/3;
 	}
 	
-	public void display(String name,int kor, int eng, int math) {
+	@Override
+	public void display() {
 		System.out.println("이름  국어 영어 수학 총점 평균");
 		System.out.println(name+" " + kor+" " + eng+"  " + math+" " + tot+" " + avg);
 	}
--- Chapter01_XML/src/main/java/sample05/SungJuk.java
+++ Chapter01_XML/src/main/java/sample05/SungJuk.java
@@ -1,6 +1,6 @@
 package sample05;
 
 public interface SungJuk {
-	public void calc(int kor, int eng, int math);
-	public void display(String name,int kor, int eng, int math);
+	public void calc();
+	public void display();
 }
